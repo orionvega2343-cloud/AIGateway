31e2153fc9d68db5d3318d151776e9c8e80d953e
orionvega2343-cloud <orionvega2343@gmail.com>
2026-07-19 23:47:42 +0300

    feat: add rate-limiter to limit AI api requests

--- internal/aiclient/rate_limiter.go
+++ internal/aiclient/rate_limiter.go
@@ -1,1 +1,62 @@
 package aiclient
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"github.com/redis/go-redis/v9"
+)
+
+type RateLimiter interface {
+	Wait(ctx context.Context) error
+}
+type ConnRedis struct {
+	redisClient *redis.Client
+	key         string
+	period      time.Duration
+	limit       int
+}
+
+func NewConnRedis(rdb *redis.Client, key string, period time.Duration, limit int) *ConnRedis {
+	return &ConnRedis{redisClient: rdb, key: key, period: period, limit: limit}
+}
+
+func (c *ConnRedis) Wait(ctx context.Context) error {
+	for {
+		rdb := c.redisClient
+
+		val, err := rdb.Incr(ctx, c.key).Result()
+		if err != nil {
+			log.Println(err)
+			return err
+		}
+
+		// val == 1 - первый запрос в окне, выставляем TTL.
+		if val == 1 {
+			_, err = rdb.Expire(ctx, c.key, c.period).Result()
+			if err != nil {
+				log.Println(err)
+				return err
+			}
+		}
+
+		// лимит превышен — ждём истечения TTL и пробуем снова.
+		if val > int64(c.limit) {
+			ttl, err := rdb.TTL(ctx, c.key).Result()
+			if err != nil {
+				log.Println(err)
+				return err
+			}
+			select {
+			case <-time.After(ttl):
+				//Если время превышено, ttl умирает,
+				//программа переходит к выполненному кейсу
+				continue
+			case <-ctx.Done():
+				return ctx.Err()
+			}
+		}
+		return nil
+	}
+}
